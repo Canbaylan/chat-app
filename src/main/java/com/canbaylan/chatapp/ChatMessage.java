226074210d178f5d7fcba9bd22aa663fadd1b9bc
canbaylan <canbaylan34@gmail.com>
2023-09-06 15:49:25 +0300

    backend service added

---
+++ src/main/java/com/canbaylan/chatapp/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.canbaylan.chatapp;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
